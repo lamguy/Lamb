eadccdabed2576b6d041d1255376dc2d3a5bfc70
Lam Nguyen <lam@mannapovllc.com>
2017-01-26 09:57:33 -0600

    Team & Users associations

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,15 +1,76 @@
 class UsersController < ApplicationController
+  before_action :set_user, only: [:show, :edit, :update, :destroy]
+
+  # GET /users
+  # GET /users.json
   def index
     @users = User.all
   end
 
+
+  # GET /users/1
+  # GET /users/1.json
   def show
   end
 
+  # GET /users/new
+  # GET /users/new.json
   def new
+    @user = User.new
   end
 
+  # GET /users/1/edit
   def edit
   end
-  
+
+  # POST /teams
+  # POST /teams.json
+  def create
+    @user = Team.new(user_params)
+
+    respond_to do |format|
+      if @team.save
+        format.html { redirect_to @team, notice: 'Team was successfully created.' }
+        format.json { render :show, status: :created, location: @team }
+      else
+        format.html { render :new }
+        format.json { render json: @team.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  # PATCH/PUT /teams/1
+  # PATCH/PUT /teams/1.json
+  def update
+    respond_to do |format|
+      if @user.update(user_params)
+        format.html { redirect_to @user, notice: 'Team was successfully updated.' }
+        format.json { render :show, status: :ok, location: @user }
+      else
+        format.html { render :edit }
+        format.json { render json: @user.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  # DELETE /teams/1
+  # DELETE /teams/1.json
+  def destroy
+    @team.destroy
+    respond_to do |format|
+      format.html { redirect_to teams_url, notice: 'Team was successfully destroyed.' }
+      format.json { head :no_content }
+    end
+  end
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_user
+      @user = User.find(params[:id])
+    end
+
+    # Never trust parameters from the scary internet, only allow the white list through.
+    def user_params
+      params.require(:user).permit(:name, team_ids: [])
+    end
 end
