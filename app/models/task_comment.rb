7309dafb6d02cf2514640f9c496e86210561dacf
Lam Nguyen <=>
2017-01-27 20:49:39 -0600

    add params for activities of task comments

--- app/models/task_comment.rb
+++ app/models/task_comment.rb
@@ -1,6 +1,6 @@
 class TaskComment < ApplicationRecord
   include PublicActivity::Model
-  tracked owner: Proc.new{ |controller, model| controller.current_user }, recipient: :task
+  tracked owner: Proc.new{ |controller, model| controller.current_user }, recipient: :task, :params => {comment: :comment}
 
   belongs_to :user
   belongs_to :task, counter_cache: true
